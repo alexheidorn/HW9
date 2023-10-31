9be15eae4e3b2db5a86c653a6dfe3369267878e1
alexheidorn <alexheidorn12@gmail.com>
2023-10-31 14:29:07 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+* Alex Heidorn
+* Created: 10/31/23
+* CS2 HW9
+*  
+*/
+
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
